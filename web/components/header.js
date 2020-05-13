1bd4cc4475db4c2da3f61b985222fd3e1c929d05
Harry-Warner <harry.e.p.warner@gmail.com>
2020-05-13 15:00:16 +0100

    fixes #8 ,  added logo and a global color object

--- web/components/header.js
+++ web/components/header.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
-import toggle from "../images/toggleicon.png";
+import colors from "../styled/colors";
+import MenuIcon from "@material-ui/icons/Menu";
+import logo from "../images/logo.png";
 
 const Header = () => {
   return (
     <StyledHeader>
-      <Logo></Logo>
+      <Logo src={logo} />
       <ToggleBtn>
-        <img className="icon" src={toggle} />
+        <MenuIcon className="icon" />
       </ToggleBtn>
     </StyledHeader>
   );
@@ -15,28 +17,27 @@ const Header = () => {
 const StyledHeader = styled.div`
   width: 100%;
   height: 30px;
-  background: green;
+  background: ${colors.green};
   position: fixed;
   display: flex;
   margin: 0;
 `;
 
-const Logo = styled.div`
+const Logo = styled.img`
   display: block;
-  margin: 0 10px;
+  margin-left: 10px;
   width: 50px;
-  height: 65px;
-  background: grey;
+  height: 70px;
 `;
 
 const ToggleBtn = styled.div`
   width: 30px;
   height: 30px;
-  background: green;
-  margin: 0 10px;
+  background: ${colors.green};
 
   .icon {
-    height: 100%;
+    padding-top: 5px;
+    height: 85%;
     width: 100%;
     background-size: cover;
     background-repeat: no-repeat;
