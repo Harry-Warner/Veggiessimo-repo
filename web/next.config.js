a41afa44b5615c792164b0c4253a7115e467921d
Harry-Warner <harry.e.p.warner@gmail.com>
2020-05-14 15:28:51 +0100

    final touchs on fonts fixes #11 fixes 13

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,3 @@
 const withImages = require("next-images");
-const compose = require("next-compose");
 
 module.exports = withImages();
--- web/styled/global.js
+++ web/styled/global.js
@@ -3,11 +3,6 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: 'playlist-script';
-    /**
-     * You don't need to use relative paths for CSS, this will be
-     * executed in the browser. The /public folder puts files at
-     * the root of the web server, so this file will be at /fonts/<filename>
-     */
     src: url('/fonts/playlist-script.ttf');
   }
   * {
@@ -19,11 +14,6 @@ const GlobalStyle = createGlobalStyle`
   };
 
   body {
-    /**
-     * Not sure if you want this on everything, everywhere,
-     * but putting it here for now
-     */
-    font-family: 'playlist-script';
     height: 100vh;
     margin: 0;
   };
