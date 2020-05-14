2b160724a088e35eaad8c0babaf22bfdd7c7978a
Matt Pocock <mattpocockvoice@gmail.com>
2020-05-14 14:43:20 +0100

    Removed next-fonts

--- web/next.config.js
+++ web/next.config.js
@@ -1,12 +1,4 @@
 const withImages = require("next-images");
-const withFonts = require("next-fonts");
 const compose = require("next-compose");
 
-module.exports = compose([
-  [withImages, withFonts],
-  {
-    webpack: (config) => {
-      return config;
-    },
-  },
-]);
+module.exports = withImages();
