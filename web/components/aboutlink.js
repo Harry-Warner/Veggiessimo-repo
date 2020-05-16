106a48e92ec5b95ce037fb978daef8c8a7e832dc
Harry-Warner <harry.e.p.warner@gmail.com>
2020-05-16 10:38:19 +0100

    example intentions on #19

--- web/components/aboutlink.js
+++ web/components/aboutlink.js
@@ -1,21 +1,47 @@
 import styled from "styled-components";
 import Colors from "../styled/colors";
+import { Component } from "react";
 
-const AboutLink = () => {
-  return (
-    <ImgBanner>
-      <TextBox>
-        <div className="columns">
-          <div className="line"></div>
-          <Text1 className="line">Cook with us</Text1>
-          <Text2 className="line">FIND OUT MORE</Text2>
-          <div className="line"></div>
-          <div className="line"></div>
-        </div>
-      </TextBox>
-    </ImgBanner>
-  );
-};
+class AboutLink extends Component {
+  constructor() {
+    super();
+    this.state = { width: 0 };
+  }
+  updateDimensions() {
+    let updateWidth = Math.min(window.innerWidth, 1024);
+    this.setState({ width: updateWidth });
+    // console.log just to check it works
+    console.log(this.state.width);
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions.bind(this));
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions.bind(this));
+  }
+  render() {
+    return (
+      <ImgBanner>
+        <TextBox>
+          <div className="columns">
+            <div className="line"></div>
+            <Text1 className="line">Cook with us</Text1>
+            <Text2 className="line">FIND OUT MORE</Text2>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+        </TextBox>
+      </ImgBanner>
+    );
+  }
+}
+
+// intention below would be to use a function of this.state.width for each width and height
+// in the elements instead of using vw. Can I pass this in with props? Reason being
+// I would like them all to change depending on device width but then the container
+// itself have a max width of 1024 (hence the 1024 in the Math.min() function).
 
 const ImgBanner = styled.div`
   display: flex;
