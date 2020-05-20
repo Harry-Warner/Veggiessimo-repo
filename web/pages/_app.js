7a5e557486c604bb6563ecdf06538559d0c41e3e
Harry-Warner <harry.e.p.warner@gmail.com>
2020-05-20 10:53:52 +0100

    bg screen width

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,14 +1,10 @@
 import React from "react";
 import App from "next/app";
 import "../styled/tailwind.css";
-import styled from "styled-components";
 import GlobalStyle from "../styled/global";
 import Head from "next/head";
 import Footer from "../components/footer";
 import Header from "../components/header";
-import cwubg640 from "../images/cwubg640.png";
-import cwubg768 from "../images/cwubg768.png";
-import cwubg1024 from "../images/cwubg1024.png";
 
 class MyApp extends App {
   render() {
@@ -47,25 +43,14 @@ class MyApp extends App {
           <meta name="theme-color" content="#d9e892" />
         </Head>
         <GlobalStyle />
-        <StyledContainer className="w-full relative min-h-screen sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-lg mx-auto pb-32 md:pb-56">
+        <div className="w-full relative min-h-screen sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-lg mx-auto pb-32 md:pb-56">
           <Header />
           <Component {...pageProps} />
           <Footer />
-        </StyledContainer>
+        </div>
       </>
     );
   }
 }
-
-const StyledContainer = styled.div`
-  background-image: url(${cwubg640});
-
-  @media (min-width: 768px) {
-    background-image: url(${cwubg768});
-  }
-  @media (min-width: 1024px) {
-    background-image: url(${cwubg1024});
-  }
-`;
 
 export default MyApp;
--- web/styled/global.js
+++ web/styled/global.js
@@ -1,4 +1,7 @@
 import { createGlobalStyle } from "styled-components";
+import cwubg640 from "../images/cwubg640.png";
+import cwubg768 from "../images/cwubg768.png";
+import cwubg1024 from "../images/cwubg1024.png";
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
@@ -16,6 +19,15 @@ const GlobalStyle = createGlobalStyle`
   body {
     height: 100vh;
     margin: 0;
+
+  background-image: url(${cwubg640});
+
+  @media (min-width: 768px) {
+    background-image: url(${cwubg768});
+  }
+  @media (min-width: 1024px) {
+    background-image: url(${cwubg1024});
+  }
   };
 `;
 
