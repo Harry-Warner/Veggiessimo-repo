343200eeb56f54f8d4bb4b261ad2a3aa6db8d969
Harry-Warner <harry.e.p.warner@gmail.com>
2020-05-21 16:34:59 +0100

    images inserted and made responsive fixes #6

--- web/components/communitylink.js
+++ web/components/communitylink.js
@@ -4,39 +4,136 @@ import styled from "styled-components";
 const CommunityLink = () => {
   return (
     <div className="block w-full">
-      <SectionOne className="w-full h-48 md:h-96 lg:h-116 bg-cover bg-center">
-        <p className="font-script text-big md:text-vbig lg:text-huge ml-2">
+      <SectionOne className="w-full relative h-48 md:h-96 lg:h-116 bg-cover bg-center">
+        <div className="bg-white absolute w-full top-0 mt-4 md:mt-8 lg:mt-12">
+          <img
+            className="w-full md:hidden"
+            src="images/com640bg.png"
+            alt="background"
+          />
+          <img
+            className="w-full hidden md:block lg:hidden"
+            src="images/com768bg.png"
+            alt="background"
+          />
+          <img
+            className="w-full hidden lg:block"
+            src="images/com1024bg.png"
+            alt="background"
+          />
+        </div>
+        <img
+          className="w-full z-10 md:hidden"
+          src="images/com640brush.png"
+          alt="banner"
+        />
+        <img
+          className="w-full z-10 hidden md:block lg:hidden"
+          src="images/com768brush.png"
+          alt="banner"
+        />
+        <img
+          className="w-full z-10 hidden lg:block"
+          src="images/com1024brush.png"
+          alt="banner"
+        />
+        <Avos
+          className="absolute md:hidden"
+          src="images/com640avos.png"
+          alt="avocados"
+        />
+        <Avos
+          className="absolute hidden md:block lg:hidden"
+          src="images/com768avos.png"
+          alt="avocados"
+        />
+        <Avos
+          className="absolute hidden lg:block"
+          src="images/com1024avos.png"
+          alt="avocados"
+        />
+        <h1 className="absolute font-script text-big md:text-huge lg:text-vhuge ml-2 z-20">
           Community
-        </p>
-        <Text1 className="text-xs md:text-lg lg:text-xxl text-center uppercase">
-          Discover more in store
+        </h1>
+        <Text1 className="absolute z-20 font-sans text-xxs md:text-lg lg:text-xxl text-center uppercase">
+          Sustainable lifestyle ideas
         </Text1>
       </SectionOne>
-      <SectionTwo className="h-40 md:h-84 lg:h-96 flex justify-end items-center bg-cover bg-center">
-        <p className="font-script text-center text-vbig md:text-vhuge leading-10 md:leading-16 mr-8 md:mr-16 lg:mr-24">
-          Bon
-          <br />
-          Apetit!
-        </p>
+      <SectionTwo className="relative">
+        <img
+          className="w-full z-10 md:hidden"
+          src="images/bonap640bg.png"
+          alt="background"
+        />
+        <img
+          className="w-full z-10 hidden md:block lg:hidden"
+          src="images/bonap768bg.png"
+          alt="background"
+        />
+        <img
+          className="w-full z-10 hidden lg:block"
+          src="images/bonap1024bg.png"
+          alt="background"
+        />
+        <Brush
+          className="absolute w-3/4 md:hidden"
+          src="images/bonap640brush.png"
+          alt="banner"
+        />
+        <Brush
+          className="absolute w-3/4 hidden md:block"
+          src="images/bonap768brush.png"
+          alt="banner"
+        />
+        <Text2 className="text-big md:text-huge absolute font-script">
+          Bon Apetit
+        </Text2>
       </SectionTwo>
     </div>
   );
 };
 
 const SectionOne = styled.div`
-  background-image: url("images/street19.jpg");
-  display: grid;
-  grid-template: 50% 50% / 50% 50%;
+  height: fit-content;
+  display: flex;
+`;
+
+const Avos = styled.img`
+  width: 55%;
+  top: 35%;
+  left: 2.5%;
+
+  @media (min-width: 1024px) {
+    width: 50%;
+    top: 30%;
+    left: 5%;
+  }
 `;
 
 const Text1 = styled.p`
-  font-family: "Fira Sans", sans-serif;
-  grid-column: 2 / 3;
-  grid-row: 2 / 3;
+  top: 50%;
+  right: 5%;
+
+  @media (min-width: 400px) and (max-width: 700px) {
+    font-size: 0.65rem;
+  }
 `;
 
 const SectionTwo = styled.div`
-  background-image: url("images/street29.jpg");
+  height: fit-content;
+`;
+
+const Brush = styled.img`
+  top: 35%;
+
+  @media (min-width: 1024px) {
+    top: 30%;
+  }
+`;
+
+const Text2 = styled.h2`
+  top: 40%;
+  left: 2.5%;
 `;
 
 export default CommunityLink;
--- web/components/recipeslink.js
+++ web/components/recipeslink.js
@@ -1,16 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
 const RecipesLink = () => {
   return (
-    <div className="w-full h-12 md:h-16 lg:h-24 my-4 bg-greenT flex">
-      <h1 className="font-script text-big md:text-vbig lg:text-huge self-center">
+    <div className="relative w-full h-12 md:h-16 lg:h-24 my-4 flex">
+      <h1 className="z-20 font-script text-big md:text-huge lg:text-vhuge self-center">
         Recipes
       </h1>
-      <h2 className="font-sans text-xxs md:text-base lg:text-xl self-end ml-2 pt-4">
+      <h2 className="z-20 font-sans text-xxs md:text-base lg:text-xl self-end ml-2 pt-4">
         DIG IN
       </h2>
+      <Brush
+        className="z-10 w-9/12 md:w-7/12 absolute"
+        src="images/recipebrush.png"
+        alt="banner"
+      />
     </div>
   );
 };
+
+const Brush = styled.img`
+  @media (min-width: 1024px) {
+    left: calc(((100vw - 1024px) / 2) * -1);
+  }
+`;
 
 export default RecipesLink;
