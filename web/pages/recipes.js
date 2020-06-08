f370ffe6ccee6ed7e46bbac7222dccb7d1e13384
Harry-Warner <harry.e.p.warner@gmail.com>
2020-06-08 23:00:39 +0100

    edits to design

--- web/pages/recipes.js
+++ web/pages/recipes.js
@@ -3,13 +3,13 @@ import List from "../components/recipelist";
 import groq from "groq";
 import client from "../client";
 import imageUrlBuilder from "@sanity/image-url";
-import styled from "styled-components";
 import TitleComponent from "../components/titleComponent.jsx";
 import RecipeList from "../styled/postlist";
 import RecipeTypes from "../components/recipetypes";
 import Footer from "../components/footer";
 import Container from "../styled/container";
 import SearchFilter from "../components/searchfilter";
+import MealTitle from "../components/mealtitle";
 
 function urlFor(source) {
   return imageUrlBuilder(client).image(source);
@@ -24,9 +24,7 @@ const Recipes = (props) => {
       <TitleComponent title="Recipes" />
       <Container>
         <RecipeTypes />
-        <StyledTitle className="w-9/12 md:w-7/12 lg:w-6/12 md:h-32 lg:h-32 mx-auto text-center my-6 flex justify-center items-center">
-          <h1 className="text-big md:text-huge font-script">Recent Recipes!</h1>
-        </StyledTitle>
+        <MealTitle title="Recent Recipes" />
         <SearchFilter posts={posts} setSearchPosts={setSearchPosts} />
         <RecipeList>
           {searchPosts &&
@@ -60,22 +58,5 @@ Recipes.getInitialProps = async () => ({
         }|order(publishedAt desc)
     `),
 });
-
-const StyledTitle = styled.div`
-  background-image: url("images/mealbrush640.png");
-  background-size: cover;
-  background-position: center;
-
-  @media (max-width: 640px) {
-    height: 22vw;
-  }
-
-  @media (min-width: 768px) and (max-width: 1023px) {
-    background-image: url("images/mealbrush768.png");
-  }
-  @media (min-width: 1024px) {
-    background-image: url("images/mealbrush1024.png");
-  }
-`;
 
 export default Recipes;
